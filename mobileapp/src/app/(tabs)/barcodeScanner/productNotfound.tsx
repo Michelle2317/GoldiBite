acb36edb727faa1f17be604a792a6fd3ec021f7b
klee616 <145302582+klee616@users.noreply.github.com>
2024-11-20 01:49:27 -0800

    create the not product found page

--- mobileapp/src/app/(tabs)/barcodeScanner/productNotfound.tsx
+++ mobileapp/src/app/(tabs)/barcodeScanner/productNotfound.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { View} from "react-native";
 import { Text } from "react-native-paper";
+import PrimaryButton from '@/src/components/paperUiElement/PrimaryButton';
+import { useRouter } from "expo-router";
 
 export default function productNotfound({}){
+    const router = useRouter();
+
+    const onPressHandles = () => {
+        router.back();
+    }
     return (<>
     <View>
-        <Text>Product Not found!!</Text>
+        <Text variant='displayMedium'>Product Not found</Text>
+        <image></image>
+        <Text variant='titleMedium'>We couldn't find any matching products</Text>
+        <PrimaryButton buttonText={"Back to Scanner"} callback={onPressHandles} />
+
     </View>
     </>)
 }

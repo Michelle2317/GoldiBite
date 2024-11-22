c2621575966dfbce71e7814ae78d2ccda5dc7fed
klee616 <145302582+klee616@users.noreply.github.com>
2024-11-21 21:34:27 -0800

    Typos in the settings page

--- mobileapp/src/components/setting/SettingBottomSheet.tsx
+++ mobileapp/src/components/setting/SettingBottomSheet.tsx
@@ -30,7 +30,7 @@ const bottomSheetBackgroundColor =  colors.mode=="dark"? "#6A6A6A":"FCE4B6";
                 handleIndicatorStyle={{ backgroundColor: "unset" }} >
                 <BottomSheetView style={{...styles.bottomSheetStyle, backgroundColor:bottomSheetBackgroundColor}}>
                 <TouchableButton name="Edit Profile" target="profileView" />
-                <TouchableButton name="Edit allergiers" target="allergy" />
+                <TouchableButton name="Sensitive ingredients" target="allergy" />
                 <TouchableButton name="Emergency contact" target="emergency" />
                 <TouchableButton name="Preferences" target="preference" />
                 <ToggleTheme />

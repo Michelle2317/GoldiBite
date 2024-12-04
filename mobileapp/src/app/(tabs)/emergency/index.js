a996fb2ea332e8f6a4a0e89fb689bc274cb7cc8d
Andrew <andrewturitsa@gmail.com>
2024-12-04 15:03:31 -0800

    adding menu and barcode scanner notice popup

--- mobileapp/src/app/(tabs)/emergency/index.js
+++ mobileapp/src/app/(tabs)/emergency/index.js
@@ -5,41 +5,43 @@ import { IconButton } from 'react-native-paper';
 import EmergencyPopup from '@/src/components/EmergencyPopup';
 
 export default function Emergency() {
-	// const [isModalVisible, setIsModalVisible] = useState(false);
-
-	// const toggleModal = () => {
-	// 	setIsModalVisible(!isModalVisible);
-	// };
     const [visible, setVisible] = useState(false);
     const showDialog = () => setVisible(true);
     const hideDialog = () => setVisible(false);
 
-	return (
-		<View style={styles.centerContainer}>
-			<IconButton
-				icon='information-outline'
-				size={35}
-				onPress={showDialog}
-				color='black'
-				style={styles.informationButton}
-			/>
-			<SosButton />
+    const popupTitle = "Why Do We Have This Option?";
+    const popupBody = `Our Call option provides immediate assistance in emergency situations, including sudden medical incidents like strokes. When clicked, the button connects users with emergency services, dialing the appropriate number based on their location (e.g., 911 in the United States). Additionally, an emergency notification is sent to emergency contacts, ensuring they are alerted in real time.`;
 
-			<EmergencyPopup visible={visible} hideDialog={hideDialog} />
-		</View>
-	);
+    return (
+        <View style={styles.centerContainer}>
+            <IconButton
+                icon='information-outline'
+                size={35}
+                onPress={showDialog}
+                color='black'
+                style={styles.informationButton}
+            />
+            <SosButton />
+            <EmergencyPopup 
+                visible={visible} 
+                hideDialog={hideDialog} 
+                title={popupTitle} 
+                body={popupBody} 
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-	centerContainer: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		position: 'relative',
-	},
-	informationButton: {
-		position: 'absolute',
-		top: -15,
-		right: 15,
-	},
+    centerContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+    },
+    informationButton: {
+        position: 'absolute',
+        top: -15,
+        right: 15,
+    },
 });
